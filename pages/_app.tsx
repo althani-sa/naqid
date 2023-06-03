02743df66bf3f6d311f21541051c780b272af834
abdulrahman-33 <white.nolan33@gmail.com>
2023-06-03 16:30:47 +0300

    a

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,7 @@ import "../styles/globals.css";
 import "@fontsource/cairo";
 import { motion } from "framer-motion";
 import Link from "next/link";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   // Global Props Context
@@ -27,6 +28,14 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <AppContext.Provider value={globalInfo}>
+      <Script id="twitterConversionCode" strategy="worker">
+        {`
+       !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+       },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
+       a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+       twq('config','of4io');
+      `}
+      </Script>
       {globalInfo.screenWidth != null && (
         <motion.main
           className="font-cairo w-screen overflow-hidden"
